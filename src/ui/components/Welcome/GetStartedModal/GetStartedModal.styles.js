c758031bf548c24bf8010ef4f5f3c4ba0c5f0151
Volodymyr Bilyk <eclipseeer@gmail.com>
2021-02-18 17:26:23 +0200

    feat(pages): Add Welcome page

--- src/ui/components/Welcome/GetStartedModal/GetStartedModal.styles.js
+++ src/ui/components/Welcome/GetStartedModal/GetStartedModal.styles.js
@@ -7,12 +7,22 @@ const styles = {
     alignItems: 'center'
   },
   container: {
-    width: 500,
+    width: 300,
     height: 300,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    flexWrap: 'wrap',
     outline: 'none'
+  },
+  iconWrapper: {
+    width: '96px',
+    height: '96px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    border: '1px solid rgba(0, 0, 0, 0.14)',
+    borderRadius: '8px'
   }
 };
 
