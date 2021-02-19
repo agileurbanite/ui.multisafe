4c667ddf2fc666d42e3fb3244246163019f9a2d6
Volodymyr Bilyk <eclipseeer@gmail.com>
2021-02-19 11:25:16 +0200

    feat(store): Add base store architecture, rename module Home to StartWork

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -1,12 +1,12 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Welcome } from './Welcome/Welcome';
-import { Home } from './Home/Home';
+import { StartWork } from './StartWork/StartWork';
 import { routes } from '../config/routes';
 
 export const App = () => (
   <Switch>
     <Redirect exact from={routes.root} to={routes.welcome} />
     <Route exact path={routes.welcome} component={Welcome} />
-    <Route exact path={[routes.getStarted, routes.createMultisafe]} component={Home} />
+    <Route exact path={[routes.getStarted, routes.createMultisafe]} component={StartWork} />
   </Switch>
 );
