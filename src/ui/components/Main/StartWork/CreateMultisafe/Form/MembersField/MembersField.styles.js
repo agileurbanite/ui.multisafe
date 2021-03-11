a0756b244db4784bc1631154bf9352a57afec836
Eugene Karakulin <coppusdavid@gmail.com>
2021-03-11 13:32:38 +0200

    feat(create-multisafe): Remove button in Members field area

--- src/ui/components/Main/StartWork/CreateMultisafe/Form/MembersField/MembersField.styles.js
+++ src/ui/components/Main/StartWork/CreateMultisafe/Form/MembersField/MembersField.styles.js
@@ -12,7 +12,17 @@ const styles = {
     width: '100%',
     marginLeft: 20
   },
-  addButton: {}
+  addButton: {},
+  iconButton: {
+    padding: '8px',
+    margin: '0 16px',
+  },
+  icon: {
+    color: '#989898',
+    '&:hover': {
+      color: '#e40029',
+    },
+  }
 };
 
 export const useStyles = makeStyles(styles, { name: 'MembersField' });
