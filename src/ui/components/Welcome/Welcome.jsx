3edd8b79206f6e1e5ceb50d5ff844928ea61f2ef
Volodymyr Bilyk <b.volodymyr@4irelabs.com>
2021-03-16 11:47:04 +0200

    feat(welcome): Add 'Try demo' button

--- src/ui/components/Welcome/Welcome.jsx
+++ src/ui/components/Welcome/Welcome.jsx
@@ -1,11 +1,18 @@
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import { Footer } from '../general/Footer/Footer';
 import { ConnectWallet } from '../general/ConnectWallet/ConnectWallet';
 import { useStyles } from './Welcome.styles';
+import { routes } from '../../config/routes';
 import logo from '../../images/logo/logo-white@3x.png';
 import laptop from '../../images/welcome-page/laptop@2x.png';
 
 export const Welcome = () => {
+  const { push } = useHistory();
   const classes = useStyles();
+
+  const tryDemo = () => push(routes.getStarted);
+
   return (
     <div className={classes.container}>
       <img className={classes.logo} src={logo} alt="Logo of Multisafe white" />
@@ -28,6 +35,9 @@ export const Welcome = () => {
             content: 'Get Started',
           }}
         />
+        <Button className={classes.tryDemo} variant="outlined" color="primary" onClick={tryDemo}>
+          Try Demo
+        </Button>
       </div>
       <img className={classes.laptop} src={laptop} alt="laptop with multisafe app" />
       <Footer classNames={{ container: classes.footer }} variant="dark" />
