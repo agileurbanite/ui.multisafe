ade9e6118b8fd557b4379fa573782046be0bf837
Volodymyr Bilyk <b.volodymyr@4irelabs.com>
2021-03-17 11:01:59 +0200

    feat(permissions): Disable Create Multisafe links when user isn't connected

--- src/ui/components/Main/StartWork/GetStarted/GetStarted.jsx
+++ src/ui/components/Main/StartWork/GetStarted/GetStarted.jsx
@@ -1,4 +1,5 @@
 import { Add, SystemUpdateAlt } from '@material-ui/icons';
+import { useStoreState } from 'easy-peasy';
 import { emoji } from '../../../../config/emoji';
 import { Headline } from '../../../general/Headline/Headline';
 import { EmojiIcon } from '../../../general/EmojiIcon/EmojiIcon';
@@ -8,6 +9,7 @@ import { routes } from '../../../../config/routes';
 import { useStyles } from './GetStarted.styles';
 
 export const GetStarted = () => {
+  const isConnected = useStoreState((store) => store.general.user.isConnected);
   const classes = useStyles();
   return (
     <div className={classes.container}>
@@ -16,16 +18,17 @@ export const GetStarted = () => {
           <EmojiIcon position="center" size={72} content={emoji.handRaised} elem="span" />
         </div>
         <div className={classes.titleBlock}>
-          <Headline is={1}>
-            Get started with Multisafe.
-          </Headline>
-          <Headline is={1}>
-            Choose one of the options.
-          </Headline>
+          <Headline is={1}>Get started with Multisafe.</Headline>
+          <Headline is={1}>Choose one of the options.</Headline>
         </div>
       </div>
       <div className={classes.multisafeActions}>
-        <GreenLink to={routes.createMultisafe} text="Create new Multi Safe" icon={Add} />
+        <GreenLink
+          to={routes.createMultisafe}
+          text="Create new Multi Safe"
+          icon={Add}
+          disabled={!isConnected}
+        />
         <GreenLink
           to={routes.loadMultisafe}
           text="Load existing Multi Safe"
