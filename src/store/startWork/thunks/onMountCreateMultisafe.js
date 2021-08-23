483a0888db8b83c1aa1e5c6bc09a36d30b03c9ed
Volodymyr Bilyk <b.volodymyr@4irelabs.com>
2021-08-23 14:55:54 +0300

    Replace custom WalletConnection and Contract with near-api-js entities

--- src/store/startWork/thunks/onMountCreateMultisafe.js
+++ src/store/startWork/thunks/onMountCreateMultisafe.js
@@ -1,5 +1,5 @@
 import { thunk } from 'easy-peasy';
-import { Contract } from '../../../near/api/Сontract';
+import { Contract } from 'near-api-js';
 import { config } from '../../../near/config';
 
 const { contractId, methods } = config.multisafeFactory;
