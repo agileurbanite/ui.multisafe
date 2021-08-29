40716c0126290584f2f9368b724d4bebed2f694a
Volodymyr Bilyk <b.volodymyr@4irelabs.com>
2021-08-29 14:49:34 +0300

    Integrate ledger into onTransferTokens

--- src/store/multisafe/thunks/onTransferTokens.js
+++ src/store/multisafe/thunks/onTransferTokens.js
@@ -1,28 +1,46 @@
 import { thunk } from 'easy-peasy';
 import { utils } from 'near-api-js';
+import { signTransactionByLedger } from '../helpers/signTransactionByLedger';
 
-export const onTransferTokens = thunk(async (_, payload, { getStoreState }) => {
+const addTransferRequest = (contract, withApprove, recipientId, amount) => {
+  const method = withApprove ? 'add_request_and_confirm' : 'add_request';
+
+  return contract[method]({
+    args: {
+      request: {
+        receiver_id: recipientId,
+        actions: [{ type: 'Transfer', amount: utils.format.parseNearAmount(amount) }],
+      },
+    },
+  });
+};
+
+const signTxByLedger = async (contract, withApprove, recipientId, amount, state, actions) => {
+  await signTransactionByLedger({
+    actionName: 'Transfer',
+    state,
+    actions,
+    contractMethod: () => addTransferRequest(contract, withApprove, recipientId, amount),
+    callback: async () => {
+      // TODO move onMountDashboard functions into helper - it can mislead devs in the future
+      await actions.multisafe.onMountDashboard();
+    },
+  });
+};
+
+export const onTransferTokens = thunk(async (_, payload, { getStoreState, getStoreActions }) => {
   const { onClose } = payload;
   const { recipientId, amount, withApprove } = payload.data;
 
-
   const state = getStoreState();
+  const isNearWallet = state.general.selectors.isNearWallet;
   const contract = state.multisafe.entities.contract;
 
-  const method = withApprove ? 'add_request_and_confirm' : 'add_request';
+  const actions = getStoreActions();
 
-  try {
-    await contract[method]({
-      args: {
-        request: {
-          receiver_id: recipientId,
-          actions: [{ type: 'Transfer', amount: utils.format.parseNearAmount(amount) }],
-        },
-      },
-    });
-  } catch (e) {
-    // throw new Error(e);
-  }
+  isNearWallet
+    ? addTransferRequest(contract, withApprove, recipientId, amount)
+    : await signTxByLedger(contract, withApprove, recipientId, amount, state, actions);
 
   onClose();
 });
