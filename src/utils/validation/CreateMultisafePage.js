78ad59807ddd862939ef7603e974c5bdf27c8c55
Volodymyr Bilyk <b.volodymyr@4irelabs.com>
2021-09-14 18:45:15 +0300

    Fix create/load multisafe layout

--- src/utils/validation/CreateMultisafePage.js
+++ src/utils/validation/CreateMultisafePage.js
@@ -15,7 +15,7 @@ const validationMessageType = {
   name: 'Name must be at least 4 characters long',
   multisafeId: 'Multisafe ID must be at least 1 character long',
   members: 'At least 1 member must be present',
-  account_id: 'Member address must contain network connection type: e.g. alice.near, alice.testnet',
+  account_id: 'Member Account ID must contain network connection type: e.g. alice.near, alice.testnet',
   num_confirmations: '',
   amount: 'Enter a valid amount. Minimum is 5 NEAR',
 };
