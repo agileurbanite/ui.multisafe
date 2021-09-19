08a39b57df2412b073b49bb8a658caf81041351d
Volodymyr Bilyk <b.volodymyr@4irelabs.com>
2021-09-19 22:04:32 +0300

    Fix: after adding new request via ledger delete button is active instead of being inactive during next 15 min

--- src/store/multisafe/thunks/onConfirmRequest.js
+++ src/store/multisafe/thunks/onConfirmRequest.js
@@ -8,15 +8,16 @@ const signTxByNearWallet = (contract, requestId) => {
   callContractChangeMethod(contract, requestId);
 };
 
-const signTxByLedger = async (contract, requestId, state, actions) => {
+const signTxByLedger = async (contract, requestId, multisafeId, state, actions) => {
   await signTransactionByLedger({
     actionName: 'Confirm Request',
     state,
     actions,
     contractMethod: () => callContractChangeMethod(contract, requestId),
     callback: async () => {
+      // Here we load data to update UI according to the last changes
       // TODO move onMountDashboard functions into helper - it can mislead devs in the future
-      await actions.multisafe.onMountDashboard();
+      await actions.multisafe.onMountDashboard(multisafeId);
     },
   });
 };
@@ -27,10 +28,11 @@ export const onConfirmRequest = thunk(async (_, payload, { getStoreState, getSto
   const state = getStoreState();
   const isNearWallet = state.general.selectors.isNearWallet;
   const contract = state.multisafe.entities.contract;
+  const multisafeId = state.multisafe.general.multisafeId;
 
   const actions = getStoreActions();
 
   isNearWallet
     ? signTxByNearWallet(contract, requestId)
-    : await signTxByLedger(contract, requestId, state, actions);
+    : await signTxByLedger(contract, requestId, multisafeId, state, actions);
 });
