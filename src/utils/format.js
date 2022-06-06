3dd9da7f34090bcdc39628a8c606b44ba493097e
Judith Lao <judith@near.org>
2022-06-06 17:48:43 -0400

    feat: ability to choose between fungible tokens you own when sending

--- src/utils/format.js
+++ src/utils/format.js
@@ -4,4 +4,5 @@ import { utils } from 'near-api-js';
 // Space 2 snake case
 export const spaceToSnake = (str) => R.compose(R.join('_'), R.split(' '), R.trim, R.toLower)(str);
 export const formatNearBalance = (balance) => `${utils.format.formatNearAmount(balance, 3)} NEAR`;
-export const formatOtherBalance = ({tokenBalance, decimals, symbol}) => `${tokenBalance* 10**-decimals} ${symbol}`
+export const formatOtherBalance = ({tokenBalance, decimals, symbol}) => `${tokenBalance* 10**-decimals} ${symbol}`;
+export const formatOtherBalanceWithoutSymbol = ({tokenBalance, decimals}) => `${tokenBalance* 10**-decimals}`;
