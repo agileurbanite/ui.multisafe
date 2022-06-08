19b80895e3551a0e4efedfe046e94f982daff80c
Judith Lao <judith@near.org>
2022-06-08 16:08:38 -0400

    feat: ablility to transfer from one multisafe to another multisafe

--- src/store/multisafe/thunks/onTransferTokens.js
+++ src/store/multisafe/thunks/onTransferTokens.js
@@ -1,21 +1,14 @@
 import * as nearApiJs from 'near-api-js';
 import { thunk } from 'easy-peasy';
 import { signTransactionByLedger } from '../helpers/signTransactionByLedger';
+import FungibleTokens from '../../../services/FungibleTokens';
+import { parseOtherAmount } from '../../../utils/format';
 
 const {
   utils: {
       format: { parseNearAmount },
   },
 } = nearApiJs;
-
-// set this to the same value as we use for creating an account and the remainder is refunded
-const FT_TRANSFER_GAS = parseNearAmount('0.00000000003');
-console.log("FT_TRANSFER_GAS",FT_TRANSFER_GAS)
-
-// contract might require an attached depositof of at least 1 yoctoNear on transfer methods
-// "This 1 yoctoNEAR is not enforced by this standard, but is encouraged to do. While ability to receive attached deposit is enforced by this token."
-// from: https://github.com/near/NEPs/issues/141
-const FT_TRANSFER_DEPOSIT = '1';
 
 const addTransferNearRequest = (contract, withApprove, recipientId, amount) => {
   const method = withApprove ? 'add_request_and_confirm' : 'add_request';
@@ -25,26 +18,6 @@ const addTransferNearRequest = (contract, withApprove, recipientId, amount) => {
       request: {
         receiver_id: recipientId,
         actions: [{ type: 'Transfer', amount: parseNearAmount(amount) }],
-      },
-    },
-  });
-};
-
-const addTransferRequest = async (contract, withApprove, recipientId, amount, contractName) => {
-  const method = withApprove ? 'add_request_and_confirm' : 'add_request';
-  const args = Buffer.from(`{"amount": ${amount}, "memo": ${undefined}, "receiver_id": ${recipientId}]}`)
-    .toString('base64')
-  return contract[method]({
-    args: {
-      request: {
-        receiver_id: contractName,
-        actions: [{
-          type: 'FunctionCall',
-          method_name: 'ft_transfer',
-          args,
-          deposit: FT_TRANSFER_DEPOSIT,
-          gas: FT_TRANSFER_GAS,
-        }],
       },
     },
   });
@@ -63,7 +36,7 @@ const signTxByLedger = async (
     actionName: 'Transfer',
     state,
     actions,
-    contractMethod: () => addTransferRequest(contract, withApprove, recipientId, amount),
+    contractMethod: () => addTransferNearRequest(contract, withApprove, recipientId, amount),
     callback: async () => {
       // Here we load data to update UI according to the last changes
       // TODO move onMountDashboard functions into helper - it can mislead devs in the future
@@ -73,27 +46,49 @@ const signTxByLedger = async (
 };
 
 export const onTransferTokens = thunk(async (_, payload, { getStoreState, getStoreActions }) => {
-  const { onClose, contractName } = payload;
+  const { onClose, token } = payload;
   const { recipientId, amount, withApprove } = payload.data;
 
   const state = getStoreState();
   const isNearWallet = state.general.selectors.isNearWallet;
+  const near = state.general.entities.near;
   const contract = state.multisafe.entities.contract;
   const multisafeId = state.multisafe.general.multisafeId;
   const actions = getStoreActions();
+  const mountDashboard = actions.multisafe.mountDashboard;
+  const fungibleTokensService = new FungibleTokens(near.connection);
 
-  const isNearContract = !contractName;
-  // contract is assumed to be 'near' if alternate contract name is not given
-  if (isNearContract) {
+  const isNearTransaction = !token;
+  // token is assumed to be 'near' if alternate token is not given
+  if (isNearTransaction) {
     isNearWallet
       ? addTransferNearRequest(contract, withApprove, recipientId, amount)
       : await signTxByLedger(contract, withApprove, recipientId, amount, multisafeId, state, actions);
   }
   else {
     isNearWallet
-      && addTransferRequest(contract, withApprove, recipientId, amount, contractName)
+      && await fungibleTokensService.addTransferRequest({multisafeContract: contract, withApprove, recipientId, amount: parseOtherAmount(token, amount), contractName: token.contractName})
       // to do:
-      // : await signTxByLedger(contract, withApprove, recipientId, amount, multisafeId, state, actions);
+      // : await signTxByLedger(fungibleTokensService, contract, withApprove, recipientId, amount, multisafeId, state, actions, contractName);
   }
+
+  const fungibleTokens = state.multisafe.general.fungibleTokens;
+
+  const updatedTokens = await Promise.all(await fungibleTokens.map(async ({contractName}) => {
+    const tokenMetadata = await contract.account.viewFunction(
+      contractName,
+      'ft_metadata'
+    );
+    const tokenBalance = await contract.account.viewFunction(
+      contractName,
+      'ft_balance_of',
+      { account_id: multisafeId }
+    );
+    return { ...tokenMetadata, tokenBalance, contractName };
+  }));
+  mountDashboard({
+    fungibleTokens: updatedTokens,
+    refreshFungibleTokensOnly: true
+  })
   onClose();
 });
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,8 +1,13 @@
 import * as R from 'ramda';
+import Big from 'big.js';
 import { utils } from 'near-api-js';
 
 // Space 2 snake case
 export const spaceToSnake = (str) => R.compose(R.join('_'), R.split(' '), R.trim, R.toLower)(str);
 export const formatNearBalance = (balance) => `${utils.format.formatNearAmount(balance, 3)} NEAR`;
 export const formatOtherBalance = ({tokenBalance, decimals, symbol}) => `${tokenBalance* 10**-decimals} ${symbol}`;
-export const formatOtherAmount = ({tokenBalance, decimals}) => `${tokenBalance* 10**-decimals}`;
+export const formatOtherAmountHumanReadable = ({tokenBalance, decimals}) => `${tokenBalance* 10**-decimals}`;
+
+// Convert human readable amount to internal indivisible units
+// export const parseOtherAmount = ({decimals}, amount) => `${amount* 10**decimals}`;
+export const parseOtherAmount = ({decimals = 18}, value) => value && Big(value).times(Big(10).pow(decimals)).toFixed();
