e32ee18bf1f756acc5ac06d14f2c1da61dfba315
Judith Lao <judith@near.org>
2022-06-09 15:30:36 -0400

    refactor: delete unused code

--- src/utils/listLikelyAssets.js
+++ src/utils/listLikelyAssets.js
@@ -1,14 +1,5 @@
 import { config } from '../near/config';
 
-export async function listLikelyNfts(accountId) {
-    // const requestOptions = {
-    //     method: 'GET',
-    //     headers: { 'Content-Type': 'application/json' },
-    // };
-    // const response = await fetch(`${config.indexerServiceUrl}/account/${accountId}/likelyNFTs`, requestOptions);
-    // return response.json();
-}
-
 export async function listLikelyTokens(accountId) {
     const requestOptions = {
         method: 'GET',
