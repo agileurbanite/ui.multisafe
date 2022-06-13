d3dec143f85fd91c029a799a917985830ffe14ae
marcinbodnar <me@marcinbodnar.com>
2022-06-13 18:41:02 +0200

    Handle showing sub item in main menu

--- src/ui/components/Main/MultiSafe/Sidebar/Navigation/Navigation.jsx
+++ src/ui/components/Main/MultiSafe/Sidebar/Navigation/Navigation.jsx
@@ -1,17 +1,23 @@
 import { useRouteMatch } from 'react-router';
 import { Item } from './Item/Item';
+import { SubItem } from './Item/SubItem';
 import { getItems } from './getItems';
 import { useStyles } from './Navigation.styles';
 
 export const Navigation = () => {
   const match = useRouteMatch();
   const classes = useStyles();
-
   const items = getItems(match);
+  
   return (
     <div className={classes.container}>
       {items.map((item) => (
-        <Item key={item.path} item={item} />
+        <div key={item.path}>
+          <Item item={item} />
+          {item.subItems?.map((subItem) => (
+            <SubItem key={`sub-${subItem.path}`} item={subItem} />
+          ))}
+        </div>
       ))}
     </div>
   );
