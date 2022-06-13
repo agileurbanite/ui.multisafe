39f19c40f450b179f92ace891b30473f2850235c
marcinbodnar <me@marcinbodnar.com>
2022-06-13 18:00:58 +0200

    Reuse MultisafeName component for edit version

--- src/ui/components/Main/FormElements/MultisafeName/MultisafeName.jsx
+++ src/ui/components/Main/FormElements/MultisafeName/MultisafeName.jsx
@@ -1,15 +1,16 @@
 import { Typography } from '@material-ui/core';
-import { TextField } from '../../../../general/TextField/TextField';
-import { BulletHeading } from '../../../../general/BulletHeading/BulletHeading';
-import { ContentSeparator } from '../../../../../general/ContentSeparator/ContentSeparator';
+import { TextField } from '../../general/TextField/TextField';
+import { BulletHeading } from '../../general/BulletHeading/BulletHeading';
+import { ContentSeparator } from '../../../general/ContentSeparator/ContentSeparator';
 
-export const MultisafeName = ({ control, classNames, hasError, errorMessage }) => (
+export const MultisafeName = ({ control, classNames, hasError, errorMessage, editVersion = false }) => (
   <>
     <BulletHeading>Multi Safe Name</BulletHeading>
-    <Typography className={classNames?.description}>
-      First, let&apos;s give your new Safe a name. This name is only stored locally and will never
-      be shared.
-    </Typography>
+    {!editVersion && 
+      <Typography className={classNames?.description}>
+        First, let&apos;s give your new Safe a name. This name is only stored locally and will never be shared.
+      </Typography>
+    }
     <section className={classNames?.createMultisafeBlock}>
       <TextField
         control={control}
@@ -28,9 +29,11 @@ export const MultisafeName = ({ control, classNames, hasError, errorMessage }) =
         }}
       />
     </section>
-    <Typography className={classNames?.description}>
-      By continuing you consent to the terms of use and privacy policy.
-    </Typography>
+    {!editVersion && 
+      <Typography className={classNames?.description}>
+        By continuing you consent to the terms of use and privacy policy.
+      </Typography>
+    }
     <ContentSeparator bg="rgba(0, 0, 0, 0.87)" height={1} margin="24px 0" />
   </>
 );
