f30848be2ca387dd7f22afb5a875393cddefc808
marcinbodnar <me@marcinbodnar.com>
2022-06-14 20:43:19 +0200

    Cleanup

--- src/ui/components/Main/MultiSafe/EditMultisafe/Form/Form.styles.js
+++ src/ui/components/Main/MultiSafe/EditMultisafe/Form/Form.styles.js
@@ -1,7 +1,6 @@
 import { makeStyles } from '@material-ui/core';
 
 const styles = {
-  form: {},
   textField: {
     fontSize: 20,
     fontWeight: 900,
--- src/ui/components/Main/StartWork/CreateMultisafe/Form/MembersField/MembersField.styles.js
+++ src/ui/components/Main/StartWork/CreateMultisafe/Form/MembersField/MembersField.styles.js
@@ -4,7 +4,6 @@ const styles = {
   container: {
     width: 620,
   },
-  addMemberInput: {},
   memberAddress: {
     width: '100%',
   },
