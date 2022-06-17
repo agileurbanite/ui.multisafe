2b89268f049d5e75d7568c9ecf380a1575c73c3b
Judith Lao <judith@near.org>
2022-06-17 11:59:32 -0400

    fix format of arrow function

--- src/ui/components/Main/MultiSafe/NonFungibleTokens/Collection/Collection.jsx
+++ src/ui/components/Main/MultiSafe/NonFungibleTokens/Collection/Collection.jsx
@@ -9,28 +9,26 @@ import SendIcon from '@material-ui/icons/Send';
 import { emoji as emojiConfig } from '../../../../../config/emoji';
 import { NFTMedia } from './NFTMedia';
 
-export const Collection = ({ nftCollection, classes, handleClick }) => { 
-  return (
-    <>
-      {nftCollection.tokens.map((nft) => (
-        <Card key={nft.metadata.title} className={classes.root}>
-          <CardHeader
-            avatar={
-              <Avatar className={classes.avatar}>
-                {emojiConfig.foxMuzzle}
-              </Avatar>
-            }
-            title={nft.metadata.title}
-            subheader={nftCollection.name}
-          />
-          <NFTMedia classes={classes} nft={nft} />
-          <CardActions disableSpacing>
-            <IconButton onClick={() => handleClick({id: nft.token_id, contract: nftCollection.contractName, title: nft.metadata.title })}>
-              <SendIcon/>
-            </IconButton>
-          </CardActions>
-        </Card>
-      ))}
-    </>
-  );
-}
+export const Collection = ({ nftCollection, classes, handleClick }) => (
+  <>
+    {nftCollection.tokens.map((nft) => (
+      <Card key={nft.metadata.title} className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {emojiConfig.foxMuzzle}
+            </Avatar>
+          }
+          title={nft.metadata.title}
+          subheader={nftCollection.name}
+        />
+        <NFTMedia classes={classes} nft={nft} />
+        <CardActions disableSpacing>
+          <IconButton onClick={() => handleClick({id: nft.token_id, contract: nftCollection.contractName, title: nft.metadata.title })}>
+            <SendIcon/>
+          </IconButton>
+        </CardActions>
+      </Card>
+    ))}
+  </>
+);
