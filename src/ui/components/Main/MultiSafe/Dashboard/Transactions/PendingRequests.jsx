6ef9dda2f81b6b8f6d86ce479ec4e8a61a6c1449
marcinbodnar <me@marcinbodnar.com>
2022-06-29 22:23:37 +0200

    Add disable batch request button

--- src/ui/components/Main/MultiSafe/Dashboard/Transactions/PendingRequests.jsx
+++ src/ui/components/Main/MultiSafe/Dashboard/Transactions/PendingRequests.jsx
@@ -5,6 +5,7 @@ import {
     TableRow,
     TableCell,
     TableBody,
+    Button,
 } from '@material-ui/core';
 import dateFormat from 'dateformat';
 import { useStoreActions, useStoreState } from 'easy-peasy';
@@ -21,6 +22,8 @@ export const PendingRequests = () => {
     const pendingRequests = useStoreState((store) => store.multisafe.dashboard.pendingRequests);
     const onConfirmRequest = useStoreActions((actions) => actions.multisafe.onConfirmRequest);
     const onDeleteRequest = useStoreActions((actions) => actions.multisafe.onDeleteRequest);
+    const toggleBatchRequestView = useStoreActions((actions) => actions.general.toggleBatchRequestView);
+    const batchRequestView = useStoreState((store) => store.general.batchRequestView);
     const classes = useStyles();
 
     const hasActiveRequests = pendingRequests.length > 0;
@@ -38,25 +41,45 @@ export const PendingRequests = () => {
         return false;
     };
 
-    const batchRequest = pendingRequests.reduce((x, request, i) => {
-        if (i && isBatchRequest(x[x.length-1], request)) {
-            const first = x.pop();
-            x.push({
-                batchRequest: true,
-                requests: [
-                    request,
-                    first
-                ]
-            });
-        } else {
-            x.push(request);
-        }
-        return x;
-    }, []);
+    const requestsList = batchRequestView
+        ? pendingRequests.reduce((x, request, i) => {
+            if (i && isBatchRequest(x[x.length-1], request)) {
+                const first = x.pop();
+                x.push({
+                    batchRequest: true,
+                    requests: [
+                        request,
+                        first
+                    ]
+                });
+            } else {
+                x.push(request);
+            }
+            return x;
+        }, [])
+        : pendingRequests;
+
+    const switchBatchTransactions = () => {
+        toggleBatchRequestView();
+    };
 
     return (
         <>
-            <h2 className={classes.header}>Pending Requests</h2>
+            <div className={classes.header}>
+                <h2 className={classes.h2}>Pending Requests</h2>
+                <Button 
+                    component="button"
+                    variant='contained'
+                    color="primary" 
+                    className={classes.button}
+                    onClick={switchBatchTransactions}
+                >
+                    {batchRequestView
+                        ? 'Disable Batch Request'
+                        : 'Enable Batch Request'
+                    }
+                </Button>
+            </div>
             {hasActiveRequests ? (
                 <TableContainer>
                     <Table>
@@ -71,10 +94,10 @@ export const PendingRequests = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {batchRequest.map((request) => request.batchRequest
+                            {requestsList.map((request, i) => request.batchRequest
                                 ? (
-                                    <Fragment key={`batch-${request.requestId}`}>
-                                        <TableRow style={{ backgroundColor: '#dff7f0'}}>
+                                    <Fragment key={`batch-${i}`}>
+                                        <TableRow className={classes.batchRequest}>
                                             <TableCell colSpan={2}>Batch Request</TableCell>
                                             <TableCell colSpan={4}>We recognize these two requests as a Batch Request, please confirm in the shown order</TableCell>
                                             {/* <BatchStatus
@@ -84,8 +107,7 @@ export const PendingRequests = () => {
                                             /> */}
                                         </TableRow>
                                         {request.requests.map((request) => (
-                                            <TableRow key={request.requestId} style={{ backgroundColor: '#dff7f0'}}>
-                                                {/* te style inline jeszcze przeniesc */}
+                                            <TableRow key={request.requestId} className={classes.batchRequest}>
                                                 <TableCell>{request.requestId}</TableCell>
                                                 <TableCell>{dateFormat(request.createdAt, 'd mmm yyyy - HH:MM')}</TableCell>
                                                 <Type type={request.type} />
