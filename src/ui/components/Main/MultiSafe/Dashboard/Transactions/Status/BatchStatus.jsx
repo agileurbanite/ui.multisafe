9f735a1bbf7905734b33e82ceb604e527f003675
marcinbodnar <me@marcinbodnar.com>
2022-06-29 23:40:09 +0200

    Prepare BatchStatus component

--- src/ui/components/Main/MultiSafe/Dashboard/Transactions/Status/BatchStatus.jsx
+++ src/ui/components/Main/MultiSafe/Dashboard/Transactions/Status/BatchStatus.jsx
@@ -1,19 +1,23 @@
-import { TableCell, IconButton } from '@material-ui/core';
-import { Done, Close } from '@material-ui/icons';
+import { TableCell } from '@material-ui/core';
+import { Done } from '@material-ui/icons';
 import cn from 'classnames';
 import { useState, useEffect } from 'react';
 
 import { config } from '../../../../../../../near/config';
+import { ConfirmModal } from '../../../EditMultisafe/ConfirmModal/ConfirmModal';
 import { useStyles } from './Status.styles';
 
 const canDeleteRequest = (createdAt) =>
     Date.now() > createdAt + config.multisafe.deleteRequestCooldown;
 
-export const BatchStatus = ({ request, onConfirmRequest, onDeleteRequest }) => {
-    const { requestId, isMember, createdAt } = request;
-    const { hasUserConfirm, totalNum, currentNum } = request.confirms;
+export const BatchStatus = ({ requests }) => {
+    const { isMember, createdAt } = requests[0];
+    const [isOpenConfirmModal, setOpenConfirmModal] = useState(false);
+    const { hasUserConfirm, totalNum, currentNum } = requests[0].confirms;
+
+
     const [canDelete, setCanDelete] = useState(canDeleteRequest(createdAt));
-    const classes = useStyles({ hasUserConfirm, canDelete, isMember });
+    const classes = useStyles({ hasUserConfirm, canDelete, isMember, hideDelete: true });
 
     useEffect(() => {
         if (!isMember || canDelete) return () => {};
@@ -33,27 +37,29 @@ export const BatchStatus = ({ request, onConfirmRequest, onDeleteRequest }) => {
         };
     }, [isMember, canDelete, createdAt]);
 
-    const confirmRequest = () => onConfirmRequest({ requestId });
-    const deleteRequest = () => onDeleteRequest({ requestId });
+    const confirmRequest = () => setOpenConfirmModal(true);
 
     return (
-        <TableCell className={classes.tableCell}>
-            <div className={classes.container}>
-                <button
-                    type="button"
-                    className={cn(classes.button, { [classes.disabledButton]: !isMember })}
-                    onClick={confirmRequest}
-                    disabled={!isMember || hasUserConfirm}
-                >
-                    <Done className={classes.doneIcon} />
-                    <span className={classes.description}>{`${currentNum}/${totalNum}`}</span>
-                </button>
-                {isMember && (
-                    <IconButton className={classes.cancel} onClick={deleteRequest} disabled={!canDelete}>
-                        <Close className={classes.cancelIcon} />
-                    </IconButton>
-                )}
-            </div>
-        </TableCell>
+        <>
+            <ConfirmModal
+                isOpenConfirmModal={isOpenConfirmModal}
+                closeRemoveModal={() => setOpenConfirmModal(false)}
+                requests={requests}
+            />
+        
+            <TableCell className={classes.tableCell}>
+                <div className={classes.container}>
+                    <button
+                        type="button"
+                        className={cn(classes.button, { [classes.disabledButton]: !isMember })}
+                        onClick={confirmRequest}
+                        disabled={!isMember || hasUserConfirm}
+                    >
+                        <Done className={classes.doneIcon} />
+                        <span className={classes.description}>{`${currentNum}/${totalNum}`}</span>
+                    </button>
+                </div>
+            </TableCell>
+        </>
     );
 };
