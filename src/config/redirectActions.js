f527885517ee660954d1f80de8be9d1105d3e2b3
marcinbodnar <me@marcinbodnar.com>
2022-06-29 23:36:46 +0200

    Add batchConfirm redirect action

--- src/config/redirectActions.js
+++ src/config/redirectActions.js
@@ -2,4 +2,5 @@ export const redirectActions = {
     connectNearWallet: 'connectNearWallet',
     createMultisafe: 'createMultisafe',
     batchRequest: 'batchRequest',
+    batchConfirm: 'batchConfirm'
 };
