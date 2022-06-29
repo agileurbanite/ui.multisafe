059f25bd1f65f4d81db5067d0989488a77c37b19
marcinbodnar <me@marcinbodnar.com>
2022-06-29 21:15:06 +0200

    Show batch request on requests list

--- src/ui/components/Main/MultiSafe/Dashboard/Transactions/PendingRequests.jsx
+++ src/ui/components/Main/MultiSafe/Dashboard/Transactions/PendingRequests.jsx
@@ -8,11 +8,13 @@ import {
 } from '@material-ui/core';
 import dateFormat from 'dateformat';
 import { useStoreActions, useStoreState } from 'easy-peasy';
+import { Fragment } from 'react';
 
 import { formatNearBalance } from '../../../../../../utils/format';
 import { Type } from '../../general/Type/Type';
 import { useStyles } from './PendingRequests.styles';
 import { Recipient } from './Recipient/Recipient';
+// import { BatchStatus } from './Status/BatchStatus';
 import { Status } from './Status/Status';
 
 export const PendingRequests = () => {
@@ -51,6 +53,7 @@ export const PendingRequests = () => {
         }
         return x;
     }, []);
+
     return (
         <>
             <h2 className={classes.header}>Pending Requests</h2>
@@ -68,20 +71,49 @@ export const PendingRequests = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {pendingRequests.map((request) => (
-                                <TableRow key={request.requestId}>
-                                    <TableCell>{request.requestId}</TableCell>
-                                    <TableCell>{dateFormat(request.createdAt, 'd mmm yyyy - HH:MM')}</TableCell>
-                                    <Type type={request.type} />
-                                    <Recipient recipient={request.recipient} />
-                                    <TableCell>{formatNearBalance(request.amount)}</TableCell>
-                                    <Status
-                                        request={request}
-                                        onConfirmRequest={onConfirmRequest}
-                                        onDeleteRequest={onDeleteRequest}
-                                    />
-                                </TableRow>
-                            ))}
+                            {batchRequest.map((request) => request.batchRequest
+                                ? (
+                                    <Fragment key={`batch-${request.requestId}`}>
+                                        <TableRow style={{ backgroundColor: '#dff7f0'}}>
+                                            <TableCell colSpan={2}>Batch Request</TableCell>
+                                            <TableCell colSpan={4}>We recognize these two requests as a Batch Request, please confirm in the shown order</TableCell>
+                                            {/* <BatchStatus
+                                                request={request.requests[0]}
+                                                onConfirmRequest={onConfirmRequest}
+                                                onDeleteRequest={onDeleteRequest}
+                                            /> */}
+                                        </TableRow>
+                                        {request.requests.map((request) => (
+                                            <TableRow key={request.requestId} style={{ backgroundColor: '#dff7f0'}}>
+                                                {/* te style inline jeszcze przeniesc */}
+                                                <TableCell>{request.requestId}</TableCell>
+                                                <TableCell>{dateFormat(request.createdAt, 'd mmm yyyy - HH:MM')}</TableCell>
+                                                <Type type={request.type} />
+                                                <Recipient recipient={request.recipient} />
+                                                <TableCell>{formatNearBalance(request.amount)}</TableCell>
+                                                <Status
+                                                    request={request}
+                                                    onConfirmRequest={onConfirmRequest}
+                                                    onDeleteRequest={onDeleteRequest}
+                                                />
+                                            </TableRow>
+                                        ))}
+                                    </Fragment>
+                                ) : (
+                                    <TableRow key={request.requestId}>
+                                        <TableCell>{request.requestId}</TableCell>
+                                        <TableCell>{dateFormat(request.createdAt, 'd mmm yyyy - HH:MM')}</TableCell>
+                                        <Type type={request.type} />
+                                        <Recipient recipient={request.recipient} />
+                                        <TableCell>{formatNearBalance(request.amount)}</TableCell>
+                                        <Status
+                                            request={request}
+                                            onConfirmRequest={onConfirmRequest}
+                                            onDeleteRequest={onDeleteRequest}
+                                        />
+                                    </TableRow>
+                                )
+                            )}
                         </TableBody>
                     </Table>
                 </TableContainer>
