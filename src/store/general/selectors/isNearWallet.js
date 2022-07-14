1576f89a013c7d7706b7daf313585a77e94a75c2
Danny Cho <hj.danny.cho@gmail.com>
2022-07-14 14:03:47 +1200

    fix: further update on supporting both near wallet and my-near wallet

--- src/store/general/selectors/isNearWallet.js
+++ src/store/general/selectors/isNearWallet.js
@@ -2,5 +2,5 @@ import { computed } from 'easy-peasy';
 
 export const isNearWallet = computed(
     [(_, store) => store.general.user.walletType],
-    (walletType) => walletType === 'my-near-wallet',
+    (walletType) => (walletType === 'my-near-wallet' || walletType === 'near-wallet'),
 );
