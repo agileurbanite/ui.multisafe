45d2652b1933d0a8ea01b68cd646cdbcda794c8f
roshaans <siddiqui.roshaan@gmail.com>
2022-07-27 14:43:14 -0500

    revert OpenInExplorer.jsx

--- src/ui/components/Main/MultiSafe/general/OpenInExplorer/OpenInExplorer.jsx
+++ src/ui/components/Main/MultiSafe/general/OpenInExplorer/OpenInExplorer.jsx
@@ -3,26 +3,22 @@ import { OpenInNew } from '@material-ui/icons';
 
 import { config } from '../../../../../../near/config';
 
-const getExplorerLink = ({ accountId, type, transactionHash }) => {
-    const getAccountInExplorerUrl =  (accountId) => `${config.explorerUrl}/accounts/${accountId}`;
-    const getTransactionInExplorerUrl = (transactionHash) => `${config.explorerUrl}/transactions/${transactionHash}`;
-
-    switch (type) {
-        case 'account':
-            return getAccountInExplorerUrl(accountId);
-        case 'transaction':
-            return getTransactionInExplorerUrl(transactionHash);
-        default:
-            return null;
+const getHref = ({ accountId, type, transactionHash }) => {
+    if (type === 'account' && accountId) {
+        return config.getCheckAccountInExplorerUrl(accountId);
     }
+    if (type === 'transaction' && transactionHash) {
+        return config.getCheckTransactionInExplorerUrl(transactionHash);
+    }
+    return null;
 };
 
 export const OpenInExplorer = ({ accountId, transactionHash, classNames, type = 'account' }) => {
-    const linkToExplorer = getExplorerLink({ accountId, transactionHash, type });
+    const href = getHref({ accountId, transactionHash, type });
 
     return (
         <IconButton className={classNames?.iconButton}>
-            <a href={linkToExplorer} target="_blank" rel="noreferrer">
+            <a href={href} target="_blank" rel="noreferrer">
                 <Tooltip title={`View ${type} in explorer`} placement="top">
                     <OpenInNew className={classNames?.icon} />
                 </Tooltip>
