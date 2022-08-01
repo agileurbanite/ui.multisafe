6806b25eeb18c36fdc0e620a4ae303f937bd4f24
Danny Cho <hj.danny.cho@gmail.com>
2022-08-02 11:59:21 +1200

    update: minor syntax improvements on Initializer component

--- src/ui/providers/Initializer/Initializer.jsx
+++ src/ui/providers/Initializer/Initializer.jsx
@@ -9,11 +9,13 @@ export const Initializer = ({ store, history, children }) => {
     const { selector, accountId, selectedWalletId } = useWalletSelector();
     // We have to wait until Easy-Peasy will put the data from the local storage
     // (private keys, user accountId etc) into the state
+    const isSignedIn = selector?.isSignedIn() || false;
     useEffect(() => {
         (async () => {
             await store.persist.resolveRehydration();
             await actions.general.onInitApp({ history, setInit, selector, accountId, selectedWalletId });
         })();
-    }, [actions.general, history, store.persist, selector.isSignedIn()]);
+    }, [actions.general, history, store.persist, accountId, selectedWalletId, selector, isSignedIn]);
+
     return isInit ? cloneElement(children, { history }) : <Loader />;
 };
